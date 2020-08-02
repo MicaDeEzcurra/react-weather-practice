fa5293298b7b22c17b4f3117b7d567d607632667
Mica de Ezcurra <micaeladezcurra@gmail.com>
2020-08-01 22:19:00 -0300

    icon variable con Icon.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import WeatherCard from "./components/WeatherCard";
 function App() {
 	return (
 		<div className="App">
-			<WeatherCard temp="30" />
-			<WeatherCard temp="10" />
+			<WeatherCard temp={-20} />
+			<WeatherCard temp={20} />
+			<WeatherCard temp={40} />
 		</div>
 	);
 }
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -6,38 +6,35 @@ import Condition from "./Condition";
 import Icon from "./Icon";
 
 const WeatherCard = (props) => {
-	{
-		/* Logica para variantes de color segun temperatura */
-	}
-
-	// Declaro las variables porque cuando compila, lee primero mi css y no las ve declaradas
 	let highColor = 0;
 	let lowColor = 0;
 	let bg = null;
 
 	if (props.temp > 12) {
-		let highColor = (1 - (props.temp - 12) / 28) * 255;
-		let lowColor = highColor - 150;
-		let bg = `linear-gradient(
+		//Hot weather
+		highColor = (1 - (props.temp - 12) / 28) * 255;
+		lowColor = highColor - 150; //generating gradient variation
+		bg = `linear-gradient(
 			to top,
 			rgb(255, ${highColor}, 0),
 			rgb(255, ${lowColor}, 0)
 		)
         `;
 	} else if (props.temp <= 12) {
-		let highColor = (1 - (props.temp + 20) / 32) * 255;
-		let lowColor = highColor - 150;
-		let bg = `linear-gradient(
+		//Cold weather
+		highColor = (1 - (props.temp + 20) / 32) * 255;
+		lowColor = highColor - 150;
+		bg = `linear-gradient(
 			to top,
 			rgb(0, ${highColor}, 255),
 			rgb(0, ${lowColor}, 255)
-		)
+        )
         `;
 	}
 
 	const Card = styled.div`
 		margin: 0 auto;
-		background: ;
+		background: ${bg};
 		width: 200px;
 		height: 240px;
 		display: flex;
@@ -48,9 +45,9 @@ const WeatherCard = (props) => {
 	`;
 
 	return (
-		<Card className="card">
+		<Card>
 			<Location />
-			<Icon />
+			<Icon condition={props.condition} />
 			<Condition />
 		</Card>
 	);
